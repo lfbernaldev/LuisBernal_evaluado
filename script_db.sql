75a269fb6506a98fd08244039125c46e635b725d
lfbernaldev <bernalwicho@gmail.com>
2025-09-30 23:31:11 -0600

    feat:Serie4 - Creación del script db en Mysql con las tabla de usuario y EstadoUsuario

---
+++ script_db.sql
@@ -0,0 +1,27 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS evaluacion_luisbernal_evaluado;
+USE evaluacion_luisbernal_evaluado;
+
+-- Crear tabla EstadoUsuario
+CREATE TABLE EstadoUsuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    clave VARCHAR(50) NOT NULL
+);
+
+-- Insertar datos iniciales en EstadoUsuario
+INSERT INTO EstadoUsuario (id, titulo, clave) VALUES
+(1, 'Activo', 'activo'),
+(2, 'Baja Permanente', 'baja');
+
+-- Crear tabla usuario
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    fecha DATE NOT NULL,
+    teléfono VARCHAR(20) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    creación DATETIME NOT NULL,
+    EstadoUsuarioId INT NOT NULL ,
+    FOREIGN KEY (EstadoUsuarioId) REFERENCES EstadoUsuario(id)
+);
